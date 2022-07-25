8ea6ed4d99aa0bce9256ba62eb715db1de7bdf44
Anslie <brantae@gmail.com>
2022-07-24 20:00:07 -0500

    commit tree c4ebc45fbf92eed9426381fe2f0b16500c8ac8a9

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    return cats.push('Ralph');
+}
+
+function destructivelyPrependCat() {
+    return cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat() {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+
+function prependCat() {
+    const addCats = ['Arnold', ...cats];
+    return addCats;
+}
+
+function removeLastCat() {
+    return cats.slice(0, 2);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
